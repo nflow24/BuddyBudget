4286fe7196f65dfb9b362532f9e47b3bc58a85e7
samanglo814 <sanglo@ufl.edu>
2026-03-07 14:15:20 -0500

    Added signup page, login routing

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,41 +1,18 @@
-import "./App.css";
-import buddyLogo from "./assets/logo.png";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <div className="phone-container">
-        <div className="top-section">
-          <div className="money money-1">$</div>
-          <div className="money money-2">$</div>
-          <div className="money money-3">$</div>
-          <div className="money money-4">$</div>
-        </div>
-
-        <div className="main-content">
-          <h2 className="welcome-text">Welcome to</h2>
-
-          <div className="logo-box">
-            <img
-              src={buddyLogo}
-              alt="BuddyBudget Logo"
-              className="logo-image"
-            />
-          </div>
-
-          <h1 className="title">
-            <span className="buddy-text">Buddy</span>
-            <span className="budget-text">Budget</span>
-          </h1>
-
-          <button className="get-started-btn">Get Started</button>
-
-          <p className="signin-text">
-            Have an account? <span>Sign In</span>
-          </p>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/client/src/pages/SignUp.jsx
@@ -0,0 +1,62 @@
+import "../App.css";
+import buddyLogo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+
+function SignUp() {
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="app">
+      <div className="phone-container">
+
+        <div className="top-section"></div>
+
+        <div className="main-content signup-content">
+
+          <h1 className="signup-title">Create An Account</h1>
+
+          {/* LOGIN LINK */}
+          <p className="login-link">
+            Already have an account?{" "}
+            <span onClick={() => navigate("/login")}>Login</span>
+          </p>
+
+          <div className="logo-box">
+            <img src={buddyLogo} className="logo-image signup-logo" />
+          </div>
+
+          <input
+            type="text"
+            placeholder="Name"
+            className="input-field"
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            className="input-field"
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="input-field"
+          />
+
+          <input
+            type="date"
+            className="input-field"
+          />
+
+          <button className="signup-btn">
+            →
+          </button>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
